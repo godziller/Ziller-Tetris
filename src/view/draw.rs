ca314a19d4632e56841435a396b18c4e6e420e24
Darren Counihan <darren@counihan.ie>
2025-09-28 16:35:51 +0100

    simple background rgba

--- src/view/draw.rs
+++ src/view/draw.rs
@@ -1,5 +1,6 @@
 use term2d::model::color::Color;
 use term2d::model::event::Event;
+use term2d::model::rect::Rect;
 use term2d::model::key::Key;
 use term2d::model::point::Point;
 use term2d::model::rgba::Rgba;
@@ -9,10 +10,24 @@ use term2d::App;
 use term2d::AppBuilder;
 
 use crate::model::Model;
+use crate::modelcomponents::constants::RGBA_BACKGROUND;
 
 pub fn view_fn(app: &App, _model: &Model, canvas: &mut HalfblockCanvas){
     canvas.clear();
     canvas.draw_pixel(&Point::new(10, 7), &Rgba::red());
+    draw_background(_model, canvas);
+
     canvas.display();
+}
+
+fn draw_background(model: &Model, canvas: &mut HalfblockCanvas){
+    canvas.draw_rect_fill(
+        &Rect {
+            pos: Point::new(0, 0),
+            size: Point::new(model.well.width + 2, model.well.height + 2),
+        },
+
+        &RGBA_BACKGROUND,
+    );
 }
 
