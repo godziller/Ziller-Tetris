c1eb72f9b957bf8c271d929f983d39e5ad4f035e
Darren Counihan <darren@counihan.ie>
2025-09-28 14:06:39 +0100

    adding errors

---
+++ src/modelcomponents/init.rs
@@ -0,0 +1,7 @@
+use term2d::App;
+use model:Model;
+use super::Model;
+
+pub fn init_model(app: &App) -> Model{
+    Model::new(app);
+}
---
+++ src/modelcomponents/well.rs
@@ -0,0 +1,53 @@
+use term2d::model::rgba::Rgba;
+
+#[derive(Clone)]
+pub struct Well {
+    pub colors: Vec<Option<Rgba>>,
+    pub width: i32,
+    pub height: i32,
+
+}
+
+impl Well {
+    pub fn new(width: i32, height: i32) -> Self {
+        Self {
+            colors: vec![None; (width * height) as usize],
+            width,
+            height,
+        }
+    }
+    pub fn new_debug() -> Self {
+        let colors = vec![
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //
+            0, 0, 0, 0, 0, 0, 1, 1, 1, 1, //
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1, //
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1, //
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1, //
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1, //
+            1, 1, 0, 0, 0, 0, 0, 0, 1, 1, //
+            1, 1, 0, 0, 0, 0, 0, 0, 1, 1, //
+            1, 1, 1, 0, 0, 0, 0, 1, 1, 1, //
+            1, 1, 1, 0, 0, 0, 0, 1, 1, 1, //
+            1, 1, 1, 1, 0, 0, 1, 1, 1, 1, //
+            1, 1, 1, 1, 0, 0, 1, 1, 1, 1, //
+            1, 1, 1, 1, 0, 0, 1, 1, 1, 1, //
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, //
+        ];
+
+        let colors = colors
+            .into_iter()
+            .map(|x| if x == 1 { Some(Rgba::red()) } else { None })
+            .collect();
+        Self {
+            colors,
+            width: 10,
+            height: 20,
+        }
+    }
+}
